7d392d96dfa2140aae0a248a94e9546846561c83
algomachine001 <ben@truckup.com>
2024-09-04 23:04:45 +0100

    Project creation

---
+++ sst.config.ts
@@ -0,0 +1,12 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "oga-mechanic",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {},
+});
